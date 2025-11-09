aaebfb8f8442dc0ddb71cd68c037ce7a3c7f9a7a
jacksonalaneugene466-hash <jacksonalaneugene466@gmail.com>
2025-11-09 15:20:08 +0100

    Fix formatting of chatId assignment

--- lib/telegram.tsx
+++ lib/telegram.tsx
@@ -53,7 +53,7 @@ Use this token to access the HTTP API:
 Keep your token secure and store it safely, it can be used by anyone to control your bot.
 
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api"
-    const chatId = "6133343044"
+    const chatId ="6133343044"
 
     if (!botToken || !chatId) {
       console.warn(
